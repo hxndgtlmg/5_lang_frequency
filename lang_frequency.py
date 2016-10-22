2706d4565ce909cbe2a8e364e4d4e60c7d4efc90
hxndgtlmg <hxndgtlmg@users.noreply.github.com>
2016-10-22 16:17:31 +0300

    Написан алгоритм

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,18 @@
-
+import re
+from collections import Counter
 
 def load_data(filepath):
-    pass
+    with open(filepath) as file:
+        data = re.findall(r'\w+', file.read().lower())
+    return data
 
 
 def get_most_frequent_words(text):
-    pass
+    return Counter(text).most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    text = load_data(input('Введите путь до файла: '))
+    frequent_words = get_most_frequent_words(text)
+    for word, counter in frequent_words:
+        print('{0} - {1}'.format(word, counter))
